33caac02600f49584c31b8c15f12e721e4580378
Parth Chavda <parthchavda.work@gmail.com>
2026-03-31 20:07:04 +0530

    feat: add bcrypt and DB insertion

--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,52 @@
 import { Router } from "express";
 import mongoose from "mongoose";
 import { userModel } from "../models/user.js";
-import { z } from zod;
+import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const authRouter = Router();
 
 //Sign up
-authRouter.post("/signup",(req,res) => {
+authRouter.post("/signup",async (req,res) => {
+    //Zod schema
     const schema = z.object({
         email: z.email("Invalid email"),
         password: z.string(),
-        firstNameL: z.string(),
+        firstName: z.string(),
         lastName: z.string()
     });
+    //Zod result
     const zodRes = schema.safeParse(req.body);
+    //Zod Check
     if(!zodRes.success){
         return res.status(404).json({
-            msg: "Error in signup req body"
+            msg: "Error in signup req body",
+            error: zodRes
         });
     }
-    const {email,password,firstName,lastName} = req.body;
 
+    const {email,password,firstName,lastName} = req.body;
+    //Password hashing
+    try{
+        const saltround = 10;
+        const hashedPassword = await bcrypt.hash(password,saltround);
+        await authRouter.create({
+            email: email,
+            password,hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        });
+
+        res.json({
+            msg: "Work Just OK"
+        });
+
+    } catch(err){
+        return res.status(404).json({
+            msg: "Error in db insertion"
+        });
+    }
+    
 
 });
 
