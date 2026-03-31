aa3ce5d874b1bce19a9333944f84a296b18edc2d
Parth Chavda <parthchavda.work@gmail.com>
2026-03-31 19:56:09 +0530

    feat: add signup zod schema

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,27 @@
 import { Router } from "express";
 import mongoose from "mongoose";
 import { userModel } from "../models/user.js";
+import { z } from zod;
 
 const authRouter = Router();
 
 //Sign up
 authRouter.post("/signup",(req,res) => {
+    const schema = z.object({
+        email: z.email("Invalid email"),
+        password: z.string(),
+        firstNameL: z.string(),
+        lastName: z.string()
+    });
+    const zodRes = schema.safeParse(req.body);
+    if(!zodRes.success){
+        return res.status(404).json({
+            msg: "Error in signup req body"
+        });
+    }
     const {email,password,firstName,lastName} = req.body;
-    res.json("IN signup page");
+
+
 });
 
 export {authRouter};
