cd6ea42a1b491717d35ea0958b05340b55dd92ee
Parth Chavda <parthchavda.work@gmail.com>
2026-04-01 00:39:51 +0530

    fix: decodeToken line

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,14 +6,17 @@ function authUser(req,res,next){
     console.log("IN Auth Middleware");
     try {
         const token = req.headers.token;
-        const decodedToken = jwt.verify(token.id,process.env.JWT_SECRET);
-        console.log(decodedToken);
+        const decodedToken = jwt.verify(token,process.env.JWT_SECRET);
+        req.userId = decodedToken.id
         next();
     } catch (err) {
         return res.json({
-            msg: "Error in auth middleware"
+            msg: "Error in auth middleware",
+            error: err
         });
     }
 
 
 }
+
+export {authUser};
