8d168474bf0f47c27d4613d421e828652929ccca
Parth Chavda <parthchavda.work@gmail.com>
2026-03-31 12:18:29 +0530

    feat: add user schema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+
+const {Schema} = mongoose;
+
+const user = new Schema({
+    email: {type: String,unique: true,required: true},
+    password: {type: String,required: true},
+    firstName: {type: String,required: true},
+    lastName: {type: String, required: true}
+});
+
+
+const userModel = mongoose.model("User",user);
+export {userModel};
