ae0d4e938e386245fa8d504ca9786b383d0f3bb0
Parth Chavda <parthchavda.work@gmail.com>
2026-04-16 09:45:17 +0530

    feat: add eRouter in app js

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ import express from "express";
 import { connectDB } from "./db.js";
 import { authRouter } from "./routes/auth.js";
 import { profileRouter } from "./routes/profile.js";
+import { eRouter } from "./routes/emergency.js";
 
 const app = express();
 app.use(express.json());
 
+app.use("/user",authRouter);
+app.use("/profile",profileRouter);
+app.use("/emergency",eRouter);
+
 try{
     await connectDB();
     app.listen(3000, ()=>{
-        console.log("Listening on port 30000");
+        console.log("Listening on port 3000");
     });
 } catch(err){
     console.log("error in app.js",err);
 }
-
-
-app.use("/user",authRouter);
-app.use("/profile",profileRouter);
